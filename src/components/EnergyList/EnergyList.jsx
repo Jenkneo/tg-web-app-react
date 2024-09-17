7f5cb5fea2f67cdede6d083e54dbc7279c5b4e1e
Evgeny <jenkneo.lal@gmail.com>
2024-09-17 20:05:10 +0400

    add fake data & some css

--- src/components/EnergyList/EnergyList.jsx
+++ src/components/EnergyList/EnergyList.jsx
@@ -6,12 +6,28 @@ const EnergyList = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        // Выполняем запрос к API
-        fetch('https://endpoint.su/api/energy')
-            .then(response => response.json())
-            .then(data => setProducts(data)) // Сохраняем данные в состоянии
-            .catch(error => console.error('Error fetching data:', error));
-    }, []); // Пустой массив в зависимости, чтобы запрос выполнен только один раз при монтировании
+        // Создаем "фейковые" данные для тестирования
+        const fakeData = [
+            { brand: 'Adrenaline', name: 'Original', rating: '★★★★★' },
+            { brand: 'Adrenaline', name: 'Game Fuel Candy', rating: '★★★★★' },
+            { brand: 'Adrenaline', name: 'Game Fuel Mango', rating: '★★★★☆' },
+            { brand: 'Burn', name: 'Сочная энергия', rating: '★★★☆☆' },
+            { brand: 'Burn', name: 'Original', rating: '★★★☆☆' },
+        ];
+
+        // Симулируем задержку запроса
+        setTimeout(() => {
+            setProducts(fakeData);
+        }, 1000); // Задержка 1 секунда для имитации запроса
+    }, []);
+
+    // useEffect(() => {
+    //     // Выполняем запрос к API
+    //     fetch('https://endpoint.su/api/energy')
+    //         .then(response => response.json())
+    //         .then(data => setProducts(data)) // Сохраняем данные в состоянии
+    //         .catch(error => console.error('Error fetching data:', error));
+    // }, []); // Пустой массив в зависимости, чтобы запрос выполнен только один раз при монтировании
 
     return (
         <div className="product-list">
